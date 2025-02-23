852ad56c32145a75469a298c4be7e415689bb7da
Nafiz Imtiaz Khan <145887236+nafirk99@users.noreply.github.com>
2025-02-23 19:05:12 +0600

    Added API Documentation

--- NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc2b9da8f05d906696c9110e67a1031da9d26a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de722fef1caa23669d29820782a7c9de0f89b0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
